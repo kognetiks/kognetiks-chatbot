dcb9dd5f7a908c63c984327739ce11b74348057a
kognetiks <stepheninsrq@gmail.com>
2023-11-23 06:19:17 -0800

    V 1.6.7 Changed up how chatbotCollapseBtn is styled

--- includes/chatbot-chatgpt-settings-skins.php
+++ includes/chatbot-chatgpt-settings-skins.php
@@ -9,30 +9,30 @@
  * @package chatbot-chatgpt
  */
 
-// FIXME - THIS IS NOT WORKING AS EXPECTED - Ver 1.6.7
+// IDEA - COMING SOON - Ver 1.6.8
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) )
 die;
 
-function chatbot_chatgpt_skins_enqueue_styles() {
+// function chatbot_chatgpt_skins_enqueue_styles() {
 
-    // DIAG - Diagnostics
-    // error_log( 'Chatbot ChatGPT: chatbot_chatgpt_skins_enqueue_styles()');
+//     // DIAG - Diagnostics
+//     // error_log( 'Chatbot ChatGPT: chatbot_chatgpt_skins_enqueue_styles()');
     
-    $primary_color = get_theme_mod('primary_color', '#000000'); // Default to black if not set
+//     $primary_color = get_theme_mod('primary_color', '#000000'); // Default to black if not set
 
-    $custom_css = "
-        .parent-class chatbot-chatgpt {
-            background-color: {$primary_color} !important;
-        }
-        .parent-class chatbot-chatgpt .chatbot-chatgpt-header {
-            background-color: {$primary_color} !important;
-        }";
-    wp_add_inline_style('chatbot-chatgpt', $custom_css);
+//     $custom_css = "
+//         .parent-class chatbot-chatgpt {
+//             background-color: {$primary_color} !important;
+//         }
+//         .parent-class chatbot-chatgpt .chatbot-chatgpt-header {
+//             background-color: {$primary_color} !important;
+//         }";
+//     wp_add_inline_style('chatbot-chatgpt', $custom_css);
 
-    // DIAG - Diagnostics
-    // error_log( 'Chatbot ChatGPT: chatbot_chatgpt_skins_enqueue_styles(): ' . $custom_css);
+//     // DIAG - Diagnostics
+//     // error_log( 'Chatbot ChatGPT: chatbot_chatgpt_skins_enqueue_styles(): ' . $custom_css);
 
-}
-add_action('wp_enqueue_scripts', 'chatbot_chatgpt_skins_enqueue_styles');
+// }
+// add_action('wp_enqueue_scripts', 'chatbot_chatgpt_skins_enqueue_styles');
