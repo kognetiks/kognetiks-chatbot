ddc12132f86f61925ad164ba9bf1da6e09b2d000
Stephen Howell <stepheninsrq@gmail.com>
2024-12-29 18:34:20 -0500

    V2.2.1 - Updated administrative links

--- includes/settings/chatbot-settings-links.php
+++ includes/settings/chatbot-settings-links.php
@@ -16,9 +16,11 @@ if ( ! defined( 'WPINC' ) ) {
 // Add link to ChatGPT options in the plugins page.
 function chatbot_chatgpt_plugin_action_links( $links ) {
 
-    $settings_link = '<a href="' . admin_url( 'options-general.php?page=chatbot-chatgpt' ) . '">' . __( 'Settings', 'chatbot-chatgpt' ) . '</a>';
-    $support_link = '<a href="' . admin_url( 'options-general.php?page=chatbot-chatgpt&tab=support' ) . '">' . __( 'Support', 'chatbot-chatgpt' ) . '</a>';
-    array_unshift( $links, $settings_link, $support_link );
+    if ( current_user_can( 'manage_options' ) ) {
+        $settings_link = '<a href="' . esc_url( wp_nonce_url( admin_url( 'admin.php?page=chatbot-chatgpt' ), 'chatbot_chatgpt_settings' ) ) . '">' . __( 'Settings', 'chatbot-chatgpt' ) . '</a>';
+        $support_link = '<a href="' . esc_url( wp_nonce_url( admin_url( 'admin.php?page=chatbot-chatgpt&tab=support' ), 'chatbot_chatgpt_support' ) ) . '">' . __( 'Support', 'chatbot-chatgpt' ) . '</a>';
+        array_unshift( $links, $settings_link, $support_link );
+    }
     return $links;
 
 }
@@ -27,8 +29,8 @@ add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'chatbot_chatg
 // Add deactivation link in the plugin row meta
 function chatbot_chatgpt_plugin_row_meta( $links, $file ) {
 
-    if ( plugin_basename( __FILE__ ) == $file ) {
-        $deactivate_link = '<a href="' . wp_nonce_url( 'plugins.php?action=deactivate&amp;plugin=' . urlencode( plugin_basename( __FILE__ ) ), 'deactivate-plugin_' . plugin_basename( __FILE__ ) ) . '">' . __( 'Deactivate', 'chatbot-chatgpt' ) . '</a>';
+    if ( plugin_basename( __FILE__ ) == $file && current_user_can( 'activate_plugins' ) ) {
+        $deactivate_link = '<a href="' . esc_url( wp_nonce_url( 'plugins.php?action=deactivate&amp;plugin=' . urlencode( plugin_basename( __FILE__ ) ), 'deactivate-plugin_' . plugin_basename( __FILE__ ) ) ) . '">' . __( 'Deactivate', 'chatbot-chatgpt' ) . '</a>';
         $links[] = $deactivate_link;
     }
     return $links;
