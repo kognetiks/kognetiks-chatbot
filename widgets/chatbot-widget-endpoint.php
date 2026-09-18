4f7d1a2f128d03a354b0aec2846df041111055d7
Stephen Howell <stepheninsrq@gmail.com>
2026-09-18 18:06:17 -0500

    V2.4.8 - Pre-release fixes continued

--- widgets/chatbot-widget-endpoint.php
+++ widgets/chatbot-widget-endpoint.php
@@ -11,32 +11,32 @@
  * @package chatbot-chatgpt
  */
 
-if ( defined( 'ABSPATH' ) ) {
-    if ( function_exists( 'chatbot_chatgpt_render_remote_widget' ) ) {
-        chatbot_chatgpt_render_remote_widget();
+if ( ! defined( 'ABSPATH' ) ) {
+    $allowed_keys = array( 'assistant', 'token', 'width', 'height', 'chatbot_prompt' );
+    $forward      = array();
+
+    foreach ( $allowed_keys as $key ) {
+        if ( ! isset( $_GET[ $key ] ) || ! is_string( $_GET[ $key ] ) ) {
+            continue;
+        }
+        $value = str_replace( array( "\r", "\n", "\0" ), '', $_GET[ $key ] );
+        if ( strlen( $value ) > 512 ) {
+            $value = substr( $value, 0, 512 );
+        }
+        $forward[ $key ] = $value;
     }
+
+    $target = '../../../../index.php?kognetiks_chatbot_widget=1';
+    if ( ! empty( $forward ) ) {
+        $target .= '&' . http_build_query( $forward, '', '&', PHP_QUERY_RFC3986 );
+    }
+
+    header( 'Location: ' . $target, true, 302 );
+    header( 'X-Robots-Tag: noindex, nofollow', true );
     exit;
 }
 
-$allowed_keys = array( 'assistant', 'token', 'width', 'height', 'chatbot_prompt' );
-$forward      = array();
-
-foreach ( $allowed_keys as $key ) {
-    if ( ! isset( $_GET[ $key ] ) || ! is_string( $_GET[ $key ] ) ) {
-        continue;
-    }
-    $value = str_replace( array( "\r", "\n", "\0" ), '', $_GET[ $key ] );
-    if ( strlen( $value ) > 512 ) {
-        $value = substr( $value, 0, 512 );
-    }
-    $forward[ $key ] = $value;
+if ( function_exists( 'chatbot_chatgpt_render_remote_widget' ) ) {
+    chatbot_chatgpt_render_remote_widget();
 }
-
-$target = '../../../../index.php?kognetiks_chatbot_widget=1';
-if ( ! empty( $forward ) ) {
-    $target .= '&' . http_build_query( $forward, '', '&', PHP_QUERY_RFC3986 );
-}
-
-header( 'Location: ' . $target, true, 302 );
-header( 'X-Robots-Tag: noindex, nofollow', true );
 exit;
